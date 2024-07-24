f18419fb6ca56fcb54aa6405e318b50a8013a6af
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-07-24 18:50:35 +0200

    windows services added

--- src/eth/ws_eth_findTokens/Program.cs
+++ src/eth/ws_eth_findTokens/Program.cs
@@ -10,7 +10,11 @@ using ws_eth_findTokens;
 using eth_shared;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "ws_eth_findTokens";
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<dbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Singleton);
@@ -34,6 +38,8 @@ builder.Services.AddHttpClient("ApiAlchemy", client =>
 
 builder.Services.AddTransient<EthApi>();
 builder.Services.AddTransient<FindTransactionService>();
+
+builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();

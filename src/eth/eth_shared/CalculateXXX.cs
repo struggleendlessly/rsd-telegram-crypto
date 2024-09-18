ed973f1bf629ce6ae87168ca4c6d7803712071fb
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-09-18 11:07:39 +0200

    eeee

---
+++ src/eth/eth_shared/CalculateXXX.cs
@@ -0,0 +1,33 @@
+﻿using api_alchemy.Eth;
+using Data;
+
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eth_shared
+{
+    public class CalculateXXX
+    {
+        private readonly ILogger logger;
+        private readonly dbContext dbContext;
+
+        public CalculateXXX(
+            ILogger<CalculateXXX> logger,
+            dbContext dbContext
+            )
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public async Task Start()
+        {
+            logger.LogInformation("CalculateXXX Start");
+        }
+    }
+}

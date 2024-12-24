c273832f6f472bc1c6158038aa172f1fddea5ad6
struggleendlessly <struggleendlessly@gmail.com>
2024-12-24 18:53:29 +0100

    1

--- src/v3/apiWebBrowserParser/Program.cs
+++ src/v3/apiWebBrowserParser/Program.cs
@@ -1,5 +1,6 @@
 using apiWebBrowserParser.models;
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +13,33 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/data", (string val) =>
+app.MapPost("/data",
+    async (
+        [FromBody] TelegramMessage message,
+        HttpContext context, 
+        telegramMessagesDB db) =>
+{
+    var ipAddress = context.Connection.RemoteIpAddress?.ToString();
+    //add ip check
+    var entity = new messagesEntity
     {
+        Message = message.Message,
+        ChatName = message.ChatName.Trim().ToLower(),
+        isSent = false
+    };
 
-    }
-);
+    db.messagesEntities.Add(entity);
+    await db.SaveChangesAsync();
+
+    // add telegram message sending
+
+});
+
 
 app.Run();
+
+public class TelegramMessage
+{
+    public string Message { get; set; }
+    public string ChatName { get; set; }
+}

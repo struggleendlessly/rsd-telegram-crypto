acf4ce8f12e0b20a10285be9836e00e7a0e412de
struggleendlessly <struggleendlessly@gmail.com>
2025-01-06 21:39:01 +0100

    tea

--- src/f#/solana/wsSwaps/scopedServices/scopedSwapsBlock.fs
+++ src/f#/solana/wsSwaps/scopedServices/scopedSwapsBlock.fs
@@ -4,25 +4,21 @@ open System
 open System.Threading
 open System.Linq
 open System.IO
+open System.Text.Json
 
 open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Options
 open Microsoft.EntityFrameworkCore
 
-open System.Text
-open System.Security.Cryptography
-
-open IScopedProcessingService
-open Extensions
-open responseSwap
-open createSeq
-
-open System.Text.Json
-open Microsoft.Extensions.Options
-open ChainSettingsOptionModule
 open dbMigration
 open dbMigration.models
+
+open createSeq
+open Extensions
 open ApiCallerSOL
 open responseGetBlockSol
+open IScopedProcessingService
+open ChainSettingsOptionModule
 
 type SwapToken = 
     { 
@@ -31,10 +27,12 @@ type SwapToken =
     mutable t1addr: string 
     mutable from: string 
     mutable to_: string 
-    mutable t0amount: float 
-    mutable t1amount: float 
-    mutable t0decimals: int 
-    mutable t1decimals: int 
+    mutable t0amountFloat: float 
+    mutable t1amountFloat: float 
+    mutable t0amountInt: uint64 
+    mutable t1amountInt: uint64 
+    mutable t0decimals: uint64 
+    mutable t1decimals: uint64 
     mutable priceTokenInSol: float 
     mutable priceSolInUsd: float 
     mutable isBuyToken: bool
@@ -46,10 +44,12 @@ let emptySwapTokens = {
     t1addr = ""
     from = ""
     to_ = ""
-    t0amount = 0.0
-    t1amount = 0.0
-    t0decimals = 0
-    t1decimals = 0
+    t0amountFloat = 0.0
+    t1amountFloat = 0.0
+    t0amountInt = 0UL
+    t1amountInt = 0UL
+    t0decimals = 0UL
+    t1decimals = 0UL
     priceTokenInSol = 0.0 
     priceSolInUsd = 0.0
     isBuyToken = false
@@ -82,12 +82,20 @@ type scopedSwapsBlock(
                 |> Async.AwaitTask
                 |> Async.map (fun x -> x.numberInt)
 
-    let md5 (input : string) : string =
-        let data = Encoding.UTF8.GetBytes(input)
-        use md5 = MD5.Create()
-        (StringBuilder(), md5.ComputeHash(data))
-        ||> Array.fold (fun sb b -> sb.Append(b.ToString("x2")))
-        |> string    
+    //let md5 (input : string) : string =
+    //    let data = Encoding.UTF8.GetBytes(input)
+    //    use md5 = MD5.Create()
+    //    (StringBuilder(), md5.ComputeHash(data))
+    //    ||> Array.fold (fun sb b -> sb.Append(b.ToString("x2")))
+    //    |> string   
+        
+    let list1andLast (lst: 'a list) =
+        match lst with
+        | [] -> []
+        | [x] -> []
+        | head :: tail -> 
+            let last = List.last tail
+            [head; last]
 
     let filterSwaps (responses: responseGetBlockSol[]) =
         let mutable resMap = Map.empty<string,(Instruction list * TokenBalance list * TokenBalance list)>
@@ -95,8 +103,8 @@ type scopedSwapsBlock(
         for response in responses do
             for transaction in response.result.transactions do
                 for innerInstruction in transaction.meta.innerInstructions do
-                    let mutable filteredInnInst = List.empty<Instruction>
-                    let metaHash = JsonSerializer.Serialize(transaction.meta) |> md5
+                    let mutable filteredInnInstTransferChecked = List.empty<Instruction>
+                    let mutable filteredInnInstTransfer = List.empty<Instruction>
 
                     for instruction in innerInstruction.instructions do
                         if String.Equals(instruction.program, "spl-token", StringComparison.OrdinalIgnoreCase)                    
@@ -105,29 +113,42 @@ type scopedSwapsBlock(
                                 |Some t when String.Equals(t.``type``, "transferChecked", StringComparison.OrdinalIgnoreCase) || 
                                              String.Equals(t.``type``, "transfer", StringComparison.OrdinalIgnoreCase) 
                                              -> 
-                                                filteredInnInst <- instruction :: filteredInnInst
-                                |_ -> ()
+                                                if String.Equals(t.``type``, "transferChecked", StringComparison.OrdinalIgnoreCase)
+                                                then
+                                                    filteredInnInstTransferChecked <- instruction :: filteredInnInstTransferChecked
+                                                else
+                                                     filteredInnInstTransfer <- instruction :: filteredInnInstTransfer
+                                | _ -> ()
 
-                    if filteredInnInst |> List.length >= 2 && 
-                       transaction.meta.err = None 
+                    let rev = filteredInnInstTransferChecked 
+                                |> List.rev 
+                                |> list1andLast 
+                                |> List.append filteredInnInstTransfer
+                                                |> List.rev 
+                                                |> list1andLast
+
+                    if transaction.meta.err = None && not (List.isEmpty rev)
                     then
-                        resMap <- Map.add transaction.transaction.signatures[0] ((filteredInnInst |> List.rev), transaction.meta.preTokenBalances, transaction.meta.postTokenBalances)resMap
+                            resMap <- Map.add 
+                                            transaction.transaction.signatures[0] 
+                                            (rev, transaction.meta.preTokenBalances, transaction.meta.postTokenBalances) 
+                                            resMap  
 
         resMap 
         |> Map.toArray 
 
 
     let parceInstructionsTransferChecked (instructions: Instruction list) =
-        let swapToken = emptySwapTokens
+        let swapToken = { emptySwapTokens with to_ = "" }
 
         if List.length instructions = 2 then
             match List.nth instructions 0, List.nth instructions 1 with
             | instr1, instr2 -> 
-                swapToken.t0amount <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
-                swapToken.t1amount <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
+                swapToken.t0amountFloat <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
+                swapToken.t1amountFloat <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
 
-                swapToken.t0decimals <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.decimals)) |> Option.defaultValue 0
-                swapToken.t1decimals <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.decimals)) |> Option.defaultValue 0
+                swapToken.t0decimals <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> uint64 tokenAmount.decimals)) |> Option.defaultValue 0UL
+                swapToken.t1decimals <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> uint64 tokenAmount.decimals)) |> Option.defaultValue 0UL
 
                 swapToken.t0addr <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.mint) |> Option.defaultValue ""
                 swapToken.t1addr <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.mint) |> Option.defaultValue ""
@@ -135,59 +156,98 @@ type scopedSwapsBlock(
                 swapToken.from <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
                 swapToken.to_ <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
 
-                //if String.Equals(swapToken.t0addr, chainSettingsOption.AddressChainCoin)
-                //then
-                //    swapToken.priceTokenInSol <- swapToken.t0amount / swapToken.t1amount
-                //    swapToken.isBuyToken <- true
-                //elif String.Equals(swapToken.t1addr, chainSettingsOption.AddressChainCoin)
-                //then
-                //    swapToken.priceTokenInSol <- swapToken.t1amount / swapToken.t0amount
-                //    swapToken.isBuySol <- true
-
-                //if String.Equals(swapToken.t0addr, chainSettingsOption.AddressStableCoin)
-                //then
-                //    swapToken.priceTokenInSol <- swapToken.t0amount / swapToken.t1amount
-                //    swapToken.isBuyToken <- true
-                //elif String.Equals(swapToken.t1addr, chainSettingsOption.AddressStableCoin)
-                //then
-                //    swapToken.priceTokenInSol <- swapToken.t1amount / swapToken.t0amount
-                //    swapToken.isBuySol <- true
-    
         swapToken
 
-    let parceInstructionsTransfer(instructions: Instruction list) =
-        let swapToken = emptySwapTokens
+    let compareArrays (pre: TokenBalance list) (post: TokenBalance list) diff =
+        let results = 
+            List.map2 (fun obj1 obj2 ->
+                if obj1.uiTokenAmount.amount - diff = obj2.uiTokenAmount.amount then Some obj1
+                else None
+            ) pre post
+            |> List.choose id
+            |> List.tryHead
+    
+        results
+
+    let parceInstructionsTransfer(instructions: Instruction list) (pre: TokenBalance list) (post: TokenBalance list)  =
+        let swapToken = { emptySwapTokens with to_ = "" }
 
         if List.length instructions = 2 then
-            match List.nth instructions 0, List.nth instructions 1 with
+            match List.item 0 instructions, List.item 1 instructions with
             | instr1, instr2 -> 
-                swapToken.t0amount <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
-                swapToken.t1amount <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.uiAmount)) |> Option.defaultValue 0.0
+                swapToken.t0amountInt <- 
+                            match instr1.parsed with
+                            | Some parsed -> parsed.info.amount
+                            | None -> 0UL
+                swapToken.t1amountInt <- 
+                            match instr2.parsed with
+                            | Some parsed -> parsed.info.amount
+                            | None -> 0UL
 
-                //swapToken.t0decimals <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.decimals)) |> Option.defaultValue 0
-                //swapToken.t1decimals <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.tokenAmount |> Option.map (fun tokenAmount -> tokenAmount.decimals)) |> Option.defaultValue 0
+                swapToken.from <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
+                swapToken.to_ <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
+        
+                let t0 = compareArrays pre post swapToken.t0amountInt
+                let t1 = compareArrays pre post swapToken.t1amountInt
 
-                //swapToken.t0addr <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.mint) |> Option.defaultValue ""
-                //swapToken.t1addr <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.mint) |> Option.defaultValue ""
+                swapToken.t0decimals <- 
+                            match t0 with
+                            | Some parsed -> parsed.uiTokenAmount.decimals
+                            | None -> 0UL
 
-                //swapToken.from <- instr1.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
-                //swapToken.to_ <- instr2.parsed |> Option.bind (fun parsed -> parsed.info.authority) |> Option.defaultValue ""
-    
+                swapToken.t1decimals <- 
+                            match t1 with
+                            | Some parsed -> parsed.uiTokenAmount.decimals
+                            | None -> 0UL
+
+                swapToken.t0addr <- 
+                            match t0 with
+                            | Some parsed -> parsed.mint
+                            | None -> ""
+
+                swapToken.t1addr <- 
+                            match t1 with
+                            | Some parsed -> parsed.mint
+                            | None -> ""
+
+                swapToken.t0amountFloat <- float swapToken.t0amountInt / (10.0 ** float swapToken.t0decimals)
+                swapToken.t1amountFloat <- float swapToken.t1amountInt / (10.0 ** float swapToken.t1decimals)
+
         swapToken
 
-    let parceInstructions (instructions:Instruction list ) = 
+    let parceInstructions (instructions:Instruction list ) (pre: TokenBalance list) (post: TokenBalance list) = 
         let res = 
             match instructions 
                   |> List.forall (fun instr -> instr.parsed 
                                                |> Option.exists (fun parsed -> parsed.info.tokenAmount.IsSome)) with
             | true -> parceInstructionsTransferChecked instructions
-            | false -> parceInstructionsTransfer instructions
+            | false -> parceInstructionsTransfer instructions pre post
         res
+
+    let additionalCalculationsSwaps (swapToken: SwapToken) = 
+        if String.Equals(swapToken.t0addr, chainSettingsOption.AddressChainCoin)
+        then
+            swapToken.priceTokenInSol <- swapToken.t0amountFloat / swapToken.t1amountFloat
+            swapToken.isBuyToken <- true
+        elif String.Equals(swapToken.t1addr, chainSettingsOption.AddressChainCoin)
+        then
+            swapToken.priceTokenInSol <- swapToken.t1amountFloat / swapToken.t0amountFloat
+            swapToken.isBuySol <- true
+
+        if String.Equals(swapToken.t0addr, chainSettingsOption.AddressStableCoin)
+        then
+            swapToken.priceTokenInSol <- swapToken.t0amountFloat / swapToken.t1amountFloat
+            swapToken.isBuyToken <- true
+        elif String.Equals(swapToken.t1addr, chainSettingsOption.AddressStableCoin)
+        then
+            swapToken.priceTokenInSol <- swapToken.t1amountFloat / swapToken.t0amountFloat
+            swapToken.isBuySol <- true
+    
+
     let processSwaps (d:(string * (Instruction list * TokenBalance list * TokenBalance list))[]) =
-        for (signature, (instructions, preTokenBalances, postTokenBalances)) in d do
-            let a = parceInstructions instructions
-            printfn "%A" signature
-        ""
+         d 
+         |> Array.map (fun (signature, (instructions, preTokenBalances, postTokenBalances)) -> parceInstructions instructions preTokenBalances postTokenBalances )
+         |> Array.iter additionalCalculationsSwaps   
 
     interface IScopedProcessingService with
         member _.DoWorkAsync(ct: CancellationToken) =

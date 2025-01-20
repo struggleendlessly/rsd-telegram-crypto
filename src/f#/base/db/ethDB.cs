6a30476b4f6bd0c51b806aa21e8e9d6aa40aae28
struggleendlessly <struggleendlessly@gmail.com>
2025-01-20 17:42:22 +0100

    aaa

--- src/f#/base/db/ethDB.cs
+++ src/f#/base/db/ethDB.cs
@@ -7,6 +7,7 @@ namespace dbMigration
 {
     public class ethDB(DbContextOptions<ethDB> options) : DbContext(options), IEthDB
     {
+        public required DbSet<TriggerHistory> triggerHistoriesEntities { get; set; }
         public required DbSet<TokenInfo> tokenInfoEntities { get; set; }
         public required DbSet<BlocksEntity> blocksEntities { get; set; }
         public required DbSet<SwapsETH_USD> swapsETH_USDEntities { get; set; }
---
+++ src/f#/common/ethCommonDB/models/TriggerHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace ethCommonDB.models
+{
+    public class TriggerHistory
+    {
+        public int Id { get; set; }
+        public int blockNumberStartInt { get; set; }
+        public int blockNumberEndInt { get; set; }
+
+        public string title { get; set; }
+    }
+}

0fbcfef23072e6b6f6de18dfee9fd658dc0da186
oleksandrdubyna88@gmail.com <oleksandrdubyna88@gmail.com>
2026-01-20 22:02:23 +0200

    feat: add user settings DB table

--- src/v3/Puppeteer.Console.BlazorUI/Models/UserSettings.cs
+++ src/v3/Puppeteer.Console.BlazorUI/Models/UserSettings.cs
@@ -2,5 +2,9 @@
 
 public class UserSettings
 {
+    public int Id { get; set; }
+    
     public bool SaveLoginData { get; set; } = true;
+
+    public string UserKey { get; set; } = string.Empty;
 }
